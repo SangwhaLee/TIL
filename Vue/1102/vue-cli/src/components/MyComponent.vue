53bfd5e2cbf6e07c8b4b4785b9f9e6c87cd0272e
SangwhaLee <sanghwa1103@gmail.com>
2022-11-02 17:42:33 +0900

    1102 vue_learned

---
+++ Vue/1102/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="border">
+    <h1>This is me</h1>
+    <MyComponentItem 
+    static-props="MyComponent에서 보낸 데이터"
+    :dynamic-props="dynamicProps"
+    @give-me-ma-money="parentGetEvent"
+    @child-input="getDynamicData"
+    />  
+  </div>  
+</template>
+
+<script>
+import MyComponentItem from '@/components/MyComponentItem'
+
+export default {
+  name: 'MyComponent',
+  components:{
+    MyComponentItem,
+  },
+  data: function() {
+    return {
+      dynamicProps: '이건 동적인 데이터!',
+    }
+  },
+  methods: {
+    parentGetEvent: function(childData) {
+      console.log(childData)
+      console.log('용돈 없어!')
+    },
+    getDynamicData: function(childInputData) {
+      console.log(`사용자가 입력한 값은 ${childInputData}입니다`)
+    }
+  }
+}
+</script>
+
+<style>
+  .border {
+    border: solid 1px black;
+  }
+</style>
---
+++ Vue/1102/vue-cli/src/components/MyComponentItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3>My Item</h3>
+    <h2>{{ staticProps }}</h2>
+    <p> {{ dynamicProps }} </p>
+    <button @click="childToParent">클릭!!</button>
+    <input 
+    type="text" 
+    v-model="childInputData"
+    @keyup.enter="childInput">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name:'MyComponentItem',
+  props:{
+    staticProps: String,
+    dynamicProps: String,
+  },
+  data: function() {
+    return {
+      childInputData: null,
+    }
+  },
+  methods:{
+    childToParent: function() {
+      this.$emit('give-me-ma-money', '나는 자식이 보낸 암살자다')
+    },
+    childInput: function() {
+      this.$emit('child-input', this.childInputData)
+      this.childInputData = null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
