6882c1101887eea36dd0be2ba32c4369697af788
SangwhaLee <sanghwa1103@gmail.com>
2022-11-07 17:35:21 +0900

    1107 learned vuex

---
+++ Vue/1107/lifecycle/src/components/ChildComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    value : {{ value }}
+    <button @click="changeValue">change value</button><br>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ChildComponent',
+  data() {
+    return {
+      value: 0,
+    }
+  },
+  methods: {
+    changeValue() {
+      this.value = this.value + 1
+    }
+  },
+  // beforeCreate() {
+  //   console.log('beforeCreate')
+  // },
+  // // vue instance가 생선된 후 호출
+  // // data, computed 들의 설정이 완료된 상태
+  // // 단 DOM에 접근할 수 없음
+  // created() {
+  //   console.log('created')
+  // },
+  // beforeMount() {
+  //   console.log('beforeMount')
+  // },
+  // mounted() {
+  //   console.log('mounted')
+  // },
+  // beforeUpdate() {
+  //   console.log('beforeUpdate')
+  // },
+  // updated() {
+  //   console.log('updated')
+  // },
+  // beforeDestroy() {
+  //   console.log('beforeDestroy')
+  // },
+  // destroyed() {
+  //   console.log('destroyed')
+  // }
+}
+</script>
+
+<style>
+
+</style>
